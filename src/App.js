b54fe0c6261aa2527457acaa1aa950d59c1e84b4
Your Name <you@163.com>
2019-06-15 11:41:08 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React,{Component} from 'react'
+import {HashRouter,BrowserRouter,Route,Switch} from 'react-router-dom'
+import Login from './pages/login/login.jsx'
+import Admin from './pages/admin/admin.jsx'
+
+export default class App extends Component{
+
+    render (){
+        return (
+         <BrowserRouter>
+             <Switch>
+                 <Route path="/login" component={Login}></Route>
+                 <Route path="/" component={Admin}></Route>
+             </Switch>
+         </BrowserRouter>
+        )
+    }
+}
